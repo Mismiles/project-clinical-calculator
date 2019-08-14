6077578b14dead8d943bb7c86da4ee6cd713b00d
EC2 Default User <ec2-user@ip-172-31-21-43.eu-west-1.compute.internal>
2019-08-14 12:51:30 +0000

    First commit

---
+++ js/calculation.js
@@ -0,0 +1,31 @@
+<script>
+        var gender, age, weight, creatinine;
+        var outputText;
+        
+        function validate() {
+        
+            // get the input
+            gender = document.forms["vanc"]["gender"].value;
+            age = document.forms["vanc"]["age"].value;
+            weight = document.forms["vanc"]["weight"].value;
+            creatinine = document.forms["vanc"]["creatinine"].value;
+            
+            // validate age, weight, creatinie
+            if (age < 18) {
+                outputText = "<em>Age</em> cannot equal less than 18!";
+            } else if (isNaN(age)) {
+                outputText = "<em>Age</em> must be a number!";
+            } else if (isNaN(weight)) {
+                outputText = "<em>Weight</em> must be a number!";
+            } else if (isNaN(creatinine)) {
+                outputText = "<em>Creatinine</em> must be a number!";
+            } else {
+                // calculate the result using x = (-b +- sqrt(b^2 - 4ac)) / 2a
+                var CrCl = ((140-age)*weight)/creatinine;
+                outputText = "The creatinine clearence = <strong>" + CrCl "</strong>";
+            }
+            
+            // output the result (or errors)
+            document.getElementById("output_text").innerHTML = outputText;
+        }
+    </script>
