558f61f65e98c112a780985c5b15221c06436772
EC2 Default User <ec2-user@ip-172-31-21-43.eu-west-1.compute.internal>
2019-09-02 18:47:24 +0000

    update

--- js/calculation.js
+++ js/calculation.js
@@ -9,6 +9,8 @@
             age = document.forms["vanc"]["age"].value;
             weight = document.forms["vanc"]["weight"].value;
             creatinine = document.forms["vanc"]["creatinine"].value;
+            // male = document.getElementById("gender").value = "M";
+            // female = document.getElementById("gender").value = "F";
             
             // validate age, weight, creatinie
             if (age < 18) {
@@ -19,15 +21,51 @@
                 outputText = "<em>Weight</em> must be a number!";
             } else if (isNaN(creatinine)) {
                 outputText = "<em>Creatinine</em> must be a number!";
-            } else {
-                // calculate the result using ((140-age)*weight)/creatinine 
-                var CrCl1 = ((140-age)*weight)/(creatinine);
-                var CrCl = CrCl1.toFixed(2);
-                outputText = "The creatinine clearence =" + CrCl + " ml/min";
+            } else { 
+                var CrCl1 = ((140-age)*weight);
             }
             
             // output the result (or errors)
             document.getElementById("output_text").innerHTML = outputText;
         }
         
+        var abw; 
+        var vDose;
         
+        function dose() {
+            //get the input
+            abw = document.forms["vanc"]["weight"].value;
+        
+            if (abw < 40) {
+                vDose = 750;
+            } else if (abw >=40 && abw<= 59) {
+                vDose = 1000;
+            } else if (abw >=60 && abw<= 90) {
+                vDose = 1500;
+            } else if (abw >59) {
+                vDose = 2000;
+            }
+            document.getElementById("output_text").innerHTML = vDose;
+        } 
+        
+        function dose() {
+            //get the input
+            abw = document.forms["vanc"]["weight"].value;
+        
+            if (abw < 40) {
+                vDose = 750;
+            } else if (abw >=40 && abw<= 59) {
+                vDose = 1000;
+            } else if (abw >=60 && abw<= 90) {
+                vDose = 1500;
+            } else if (abw >59) {
+                vDose = 2000;
+            }
+            document.getElementById("output_text").innerHTML = vDose;
+        } 
+        
+        
+        
+        function getAdvice () {
+            
+        }
