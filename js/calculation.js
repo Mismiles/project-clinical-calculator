328d56a0b7fef374ac640303a0248fb70b0b7ce7
EC2 Default User <ec2-user@ip-172-31-21-43.eu-west-1.compute.internal>
2019-09-12 13:25:11 +0000

    updated formulas and table box classes

--- js/calculation.js
+++ js/calculation.js
@@ -1,19 +1,29 @@
+    //declare global variables
+    var age = document.forms["vanc"]["age"].value;
+    var weight = document.forms["vanc"]["weight"].value;
+    var height = document.forms["vanc"]["height"].value;
+    var creatinine = document.forms["vanc"]["creatine"].value;
+    var male = document.forms["vanc"]["gender"].male;
+    var female = document.forms["vanc"]["gender"].female;
+    var bmi = weight/(height/100*height/100);
+
+
         
-        // Stat dose calculation
-        var abw; 
+        // Initial dose calculation
         var vDose;
         
         function dose() {
             //get the input
-            abw = document.forms["vanc"]["weight"].value;
-        
-            if (abw < 40) {
+            
+            if (isNaN(weight)) {
+                outputText = "<em>Weight</em>must be a number!";
+            } else if (weight < 40) {
                 vDose = 750;
-            } else if (abw >=40 && abw<= 59) {
+            } else if (weight >=40 && weight<= 59) {
                 vDose = 1000;
-            } else if (abw >=60 && abw<= 90) {
+            } else if (weight >=60 && weight<= 90) {
                 vDose = 1500;
-            } else if (abw >59) {
+            } else if (weight >59) {
                 vDose = 2000;
             }
             document.getElementById("output_text").innerHTML = vDose;
@@ -21,15 +31,9 @@
         
         
         // Crcl without gender or creatinine adjustment
-        var age, weight;
         var outputText;
         
         function CrCl0() {
-        
-            // get the input
-            age = document.forms["vanc"]["age"].value;
-            creatinine = document.forms["vanc"]["creatine"].value;
-            weight = document.forms["vanc"]["weight"].value;
             // male = document.getElementById("gender").value = "M";
             // female = document.getElementById("gender").value = "F";
             
@@ -52,7 +56,6 @@
         
         // CrCl with gender adjustment
         function kidneygender(){
-        var gender = document.forms["vanc"]["creatine"].value;
         var resultG;
             if (gender === M){
                 resultG = CrCl0 *1.23;
@@ -70,32 +73,39 @@
         
         //Calculate of BMI
         function bmi() {
-            var weight = document.forms["vanc"]["weight"].value;
-            var height = document.forms["vanc"]["height"].value;
             var bmi = weight/(height/100*height/100)
-            
         }
         
-        //If overweight 
-        function overweight () {
-            var bmi = document.getElementById(bmi);
-            
-            if (bmi > 25) {
-                outputText = "The patient is obese";
+        //If overweight, calculate IBW
+        function overWeight () {
+            if (bmi > 25 && male) {
+                 50+(0.91*(height-152.4));
             } 
-            else if (isNaN(age)) {
-                outputText = "<em>Age</em> must be a number!";
-            } else if (isNaN(weight)) {
-                outputText = "<em>Weight</em> must be a number!";
-            } else if (isNaN(creatinine)) {
-                outputText = "<em>Creatinine</em> must be a number!";
-            } else { 
-                var CrCl0 = ((140-age)*weight);
+            else if (bmi > 25) {
+                 45+(0.91*(height-152.4));
             }
         }
         
-
+        //If Underweight, adjust CrCl
+        function underweight(){
+            underweight = CrCl*0.69;
+        }
         
+
+    
+    // date picker
+    
+     $(document).ready(function(){
+      var date_input=$('input[name="date"]'); //our date input has the name "date"
+      var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+      var options={
+        format: 'mm/dd/yyyy',
+        container: container,
+        todayHighlight: true,
+        autoclose: true,
+      };
+      date_input.datepicker(options);
+    })
         
         
         
