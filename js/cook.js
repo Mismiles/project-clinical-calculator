b9fc997b1d0bd43fd7fc9ca0f0efcb3c5d7ccf19
EC2 Default User <ec2-user@ip-172-31-21-43.eu-west-1.compute.internal>
2019-09-18 10:43:36 +0000

    Updated BMI calculator and spacing

--- js/cook.js
+++ js/cook.js
@@ -1,11 +1,10 @@
 //declare global variables
-    var w = document.getElementById('weight');
-    var a = window.document.getElementById('age');
-    var h = window.document.getElementById('height');
-    var c = window.document.getElementById('creatinine');
-    var m = document.forms["vanc"]["male"];
-    var f = document.forms["vanc"]["female"];
-    
+    var age = window.document.getElementById("age").value;
+    var weight = window.document.getElementById("weight").value;
+    var height = window.document.getElementById("height").value;
+    var creatinine = window.document.getElementById("creatinine").value;
+    var male = document.forms["vanc"]["male"].checked;
+    var female = document.forms["vanc"]["female"].checked;
     
     console.log('connected');
     
@@ -43,37 +42,27 @@
         
         function dose() {
             //get the input
-            var weight = w.value;
             
             if (isNaN(weight)) {
                 vDose = "<em>Weight</em>must be a number!";
             } else if (weight < 40) {
-                vDose = "750"+"mg";
+                vDose = 750+"mg";
             } else if (weight >=40 && weight<= 59) {
-                vDose = "1000"+"mg";
+                vDose = 1000+"mg";
             } else if (weight >=60 && weight<= 90) {
-                vDose = "1500"+"mg";
+                vDose = 1500+"mg";
             } else if (weight >90) {
-                vDose = "2000"+"mg";
+                vDose = 2000+"mg";
             }
-            document.getElementById("stat_dose").innerHTML = "Prescribe STAT dose of " + "<b>" + vDose + "<b>";
+            document.getElementById("stat_dose").innerHTML = "Prescribe a stat dose of " + vDose;
         }        
-        
-        
-        // Weight must be within three months
-
-        
         
         
         // Crcl without gender or creatinine adjustment
         var CrCl0;
         
         function CrCl0() {
-            //get age, weight, creatinie
-            var weight = w.value;
-            var age = a.value;
-            var creatinine = c.value;
-            
+            // validate age, weight, creatinie
             if (age < 18) {
                 CrCl0 = "<em>Age</em> cannot equal less than 18!";
             } else if (isNaN(age)) {
@@ -83,42 +72,51 @@
             } else if (isNaN(creatinine)) {
                 CrCl0 = "<em>Creatinine</em> must be a number!";
             } else { 
-                CrCl0 = ((140-age)*weight);
+                var CrCl0 = ((140-age)*weight);
             }
+            
+            // output the result (or errors)
+            document.getElementById("Cr_Cl0").innerHTML = CrCl0;
         }
         
         // CrCl with gender adjustment
-        
         function kidneygender(){
         var resultG;
-        var male = m.checked;
             if (male){
                 resultG = CrCl0 *1.23;
             } else{
                 resultG = CrCl0 *1.04;
             }
-        document.getElementById("result_g").innerHTML = "The CrCl =  " + "<b>" + resultG + "<b>";
+            
+            
+        // CrCl with creatinine
+        function CrCl(){
+        var creatinine = document.forms["vanc"]["creatine"].value;
+        var CrCl = resultG/creatinine;
         }
+        
         
         //Calculate of BMI
         var bmi;
         function bmi() {
-            var weight = w.value;
-            var height = h.value;
-            var normalweight = document.getElementById('normalweight')
-            
-            if (isNaN(weight)) {
-                bmi = "<em>Weight</em>must be a number!";
-            }else if (isNaN(height)) {
-                bmi = "<em>Height</em>must be a number!";
-            }else if (normalweight.checked) {
-                bmi = "The patient's BMI is  ";
-            }else {
-                bmi = weight/(height/100*height/100);
-        }
-        document.getElementById("bmi_calc").innerHTML = "The patient's BMI is  " + "<b>" + bmi + "<b>";
+            var bmi = weight/(height/100*height/100)
         }
         
+        //If overweight, calculate IBW
+        function overWeight () {
+            if (bmi > 25 && male) {
+                 50+(0.91*(height-152.4));
+            } 
+            else if (bmi > 25) {
+                 45+(0.91*(height-152.4));
+            }
+        }
         
+        //
         
-        
+        //If Underweight, adjust CrCl
+        function underweight(){
+            var underweight;
+            underweight = CrCl*0.69;
+        }
+    }
